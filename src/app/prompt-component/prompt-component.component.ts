71a2055a62f8862c37eb4f2055ed2552f131041f
Gagan Suneja <gagan91suneja@gmail.com>
2021-12-15 23:42:26 +0530

    Add files via upload

---
+++ src/app/prompt-component/prompt-component.component.ts
@@ -0,0 +1,23 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_BOTTOM_SHEET_DATA, MatBottomSheetRef } from '@angular/material/bottom-sheet';
+@Component({
+  selector: 'app-prompt-component',
+  templateUrl: './prompt-component.component.html',
+  styleUrls: ['./prompt-component.component.scss']
+})
+export class PromptComponent {
+
+  constructor(
+    @Inject(MAT_BOTTOM_SHEET_DATA) public data: { mobileType: 'ios' | 'android' | 'web', promptEvent?: any },
+    private bottomSheetRef: MatBottomSheetRef<PromptComponent>
+  ) {}
+
+  public installPwa(): void {
+    this.data.promptEvent.prompt();
+    this.close();
+  }
+
+  public close() {
+    this.bottomSheetRef.dismiss();
+  }
+}
